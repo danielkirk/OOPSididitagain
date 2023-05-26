6f8d052399ab035dca7a8f8012fb6c546ecdca55
Daniel Kirk <imdanielkirk@gmail.com>
2023-05-25 21:58:54 -0700

    Created indexing model to utilize possible additional fields needed for dictionary object.

---
+++ OOPSididitagain/IndexObject.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class IndexObject
+{
+    private readonly Dictionary<string, string> _dictionary;
+    public DateTime Expiry { get; set; }
+    public IndexObject()
+    {
+        _dictionary = new Dictionary<string, string>();
+    }
+
+    public string this[string Key]
+    {
+        get { return _dictionary[Key]; }
+        set { _dictionary[Key] = value; }
+    }
+}
--- OOPSididitagain/Program.cs
+++ OOPSididitagain/Program.cs
@@ -3,3 +3,7 @@ Console.WriteLine("Hello, World!");
 
 var person = new Person(new DateTime(1996, 04, 24));
 Console.WriteLine(person.Age);
+
+var cookie = new IndexObject();
+cookie["name"] = "Daniel";
+Console.WriteLine(cookie["name"]);
